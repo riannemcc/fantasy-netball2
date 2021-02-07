1380c7c3e13d09f8cdd03287cc1fad374ae532a6
Rianne <rianne.mccartney@gousto.co.uk>
2021-02-07 10:48:14 +0000

    Updating profile page

--- pages/profile.js
+++ pages/profile.js
@@ -1,32 +1,47 @@
-import { useSession } from "next-auth/client";
-import dynamic from "next/dynamic";
+import { useSession, getSession } from "next-auth/client";
+import { ObjectId } from "mongodb";
 import Link from "next/link";
 import { connectToDatabase } from "../util/mongodb";
+import { LeaderboardTable } from "../src/components/LeaderboardTable";
+import { GameSchedule } from "../src/components/GameSchedule/GameSchedule";
 
-export default function Profile({ players, users }) {
+export default function Profile({ players, users, currentUser }) {
   const [session, loading] = useSession();
 
   if (typeof window !== "undefined" && loading) return <p>Loading...</p>;
 
-  // use session.user to match users bd collection, and extract team name
-  // object of all games with dates, if date has passed, do not show game
   return (
     <div>
-      {/* <h2>{user.teamname && user.teamname}</h2> */}
-      <Link href="/team-selection">
-        <a class="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-pink font-bold items-center justify-center">
-          Team Selection
-        </a>
-      </Link>
-      <div>LEADERBOARD - TOP 10?</div>
-      <div>UPCOMING GAMES?</div>
+      <h2 className="text-xl text-black font-bold uppercase m-2">
+        {currentUser.teamname && currentUser.teamname}
+      </h2>
+      {currentUser.team ? null : (
+        <Link href="/team-selection">
+          <a class="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-pink font-bold items-center justify-center">
+            Team Selection
+          </a>
+        </Link>
+      )}
+      <h2 className="text-xl text-black font-bold uppercase m-2">
+        Top 10 Fantasy Teams
+      </h2>
+      <LeaderboardTable users={users} players={players} tenRows />
+      <h2 className="text-xl text-black font-bold uppercase m-2">
+        Upcoming Games
+      </h2>
+      <GameSchedule />
       <div>HIGHEST SCORING PLAYER</div>
     </div>
   );
 }
 
-export async function getServerSideProps() {
+export async function getServerSideProps({ req }) {
   const { db } = await connectToDatabase();
+  const session = await getSession({ req });
+
+  const currentUser = await db
+    .collection("users")
+    .findOne(ObjectId(session.userId));
 
   const users = await db
     .collection("users")
@@ -45,6 +60,7 @@ export async function getServerSideProps() {
 
   return {
     props: {
+      currentUser: JSON.parse(JSON.stringify(currentUser)),
       users: JSON.parse(JSON.stringify(users)),
       players: JSON.parse(JSON.stringify(players)),
     },
---
+++ src/components/GameSchedule/GameSchedule.js
@@ -0,0 +1,155 @@
+import React from "react";
+import Moment from "react-moment";
+
+const games = [
+  {
+    time: "2021-02-12T17:15+0000",
+    teamOne: "Celtic Dragons",
+    teamTwo: "Leeds Rhinos",
+  },
+  {
+    time: "2021-02-12T19:15+0000",
+    teamOne: "Strathclyde Sirens",
+    teamTwo: "Manchester Thunder",
+  },
+  {
+    time: "2021-02-13T14:00+0000",
+    teamOne: "London Pulse",
+    teamTwo: "Surrey Storm",
+  },
+  {
+    time: "2021-02-13T16:00+0000",
+    teamOne: "Team Bath",
+    teamTwo: "Celtic Dragons",
+  },
+  {
+    time: "2021-02-13T14:00+0000",
+    teamOne: "London Pulse",
+    teamTwo: "Surrey Storm",
+  },
+  {
+    time: "2021-02-13T18:00+0000",
+    teamOne: "Strathclyde Sirens",
+    teamTwo: "Severn Stars",
+  },
+  {
+    time: "2021-02-14T14:00+0000",
+    teamOne: "Team Bath",
+    teamTwo: "Loughborough Lightning",
+  },
+  {
+    time: "2021-02-14T16:00+0000",
+    teamOne: "Surrey Storm",
+    teamTwo: "Saracens Mavericks",
+  },
+  {
+    time: "2021-02-14T18:00+0000",
+    teamOne: "Severn Stars",
+    teamTwo: "Wasps",
+  },
+  {
+    time: "2021-02-15T17:15+0000",
+    teamOne: "Loughborough Lightning",
+    teamTwo: "Wasps",
+  },
+  {
+    time: "2021-02-15T19:15+0000",
+    teamOne: "Leeds Rhinos",
+    teamTwo: "Saracens Mavericks",
+  },
+  {
+    time: "2021-02-21T12:00+0000",
+    teamOne: "Surrey Storm",
+    teamTwo: "Leeds Rhinos",
+  },
+  {
+    time: "2021-02-21T14:00+0000",
+    teamOne: "Loughborough Lightning",
+    teamTwo: "Saracens  Mavericks",
+  },
+  {
+    time: "2021-02-21T16:00+0000",
+    teamOne: "Celtic Dragons",
+    teamTwo: "Strathclyde Sirens",
+  },
+  {
+    time: "2021-02-21T18:00+0000",
+    teamOne: "Manchester Thunder",
+    teamTwo: "London Pulse",
+  },
+  {
+    time: "2021-02-22T17:15+0000",
+    teamOne: "Celtic Dragons",
+    teamTwo: "Surrey Storm",
+  },
+  {
+    time: "2021-02-22T19:15+0000",
+    teamOne: "Wasps",
+    teamTwo: "Team Bath",
+  },
+  {
+    time: "2021-02-23T12:00+0000",
+    teamOne: "Severn Stars",
+    teamTwo: "Manchester Thunder",
+  },
+  {
+    time: "2021-02-23T14:00+0000",
+    teamOne: "Strathclyde Sirens",
+    teamTwo: "London Pulse",
+  },
+  {
+    time: "2021-02-23T16:00+0000",
+    teamOne: "Wasps",
+    teamTwo: "Surrey Storm",
+  },
+  {
+    time: "2021-02-23T18:00+0000",
+    teamOne: "Leeds Rhinos",
+    teamTwo: "Loughborough Lightning",
+  },
+  {
+    time: "2021-03-01T17:15+0000",
+    teamOne: "Celtic  Dragons",
+    teamTwo: "Saracens Mavericks",
+  },
+  {
+    time: "2021-03-01T19:15+0000",
+    teamOne: "Loughborough Lightning",
+    teamTwo: "Severn Stars",
+  },
+];
+
+const currentDate = new Date();
+
+export const GameSchedule = () => {
+  return (
+    <div>
+      <table class="table-auto ml-2">
+        <thead>
+          <tr>
+            <th class="border px-4 py-2">Date</th>
+            <th class="border px-4 py-2">Time</th>
+            <th class="border px-4 py-2">Teams</th>
+          </tr>
+        </thead>
+        <tbody>
+          {games.map(({ time, teamOne, teamTwo }) =>
+            time > currentDate ? (
+              <tr>
+                <td class="border px-4 py-2">
+                  {<Moment format="DD/MM/YYYY">{time}</Moment>}
+                </td>
+                <td class="border px-4 py-2">
+                  {<Moment format="HH:mm">{time}</Moment>}
+                </td>
+                <td class="border px-4 py-2">
+                  {teamOne} vs. {teamTwo}
+                </td>
+              </tr>
+            ) : null
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
