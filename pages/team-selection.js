e3a1ccab8d0758ef9ca9ddf62259ac28ddf4b329
Michael Roger <michael.roger@ovoenergy.com>
2021-02-12 16:50:54 +0000

    Fix teamselection message logic.

--- pages/team-selection.js
+++ pages/team-selection.js
@@ -3,45 +3,56 @@ import { connectToDatabase } from "../util/mongodb";
 import Router from "next/router";
 import { ObjectId } from "mongodb";
 import { useSession, getSession } from "next-auth/client";
+import { TeamSelection } from "../src/components/TeamSelection";
 
-export default function TeamSelection ( { players = [], currentUser } ) {
-
-
+export default function TeamSelectionPage({ players = [], currentUser }) {
   return (
     <>
-      {currentUser && currentUser.team ?
-        <TeamSelection players={ players } /> :
-        <div className="m-6 p-2 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-          You have already selected your team and cannot amend until the mid-season swap window.
+      {currentUser && currentUser.team ? (
+        <div
+          className="m-6 p-2 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+          role="alert"
+        >
+          You have already selected your team and cannot amend until the
+          mid-season swap window.
           <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-            <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /></svg>
+            <svg
+              class="fill-current h-6 w-6 text-red-500"
+              role="button"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <title>Close</title>
+              <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+            </svg>
           </span>
         </div>
-
-      }
+      ) : (
+        <TeamSelection players={players} />
+      )}
     </>
   );
 }
 
-export async function getServerSideProps ( { req } ) {
+export async function getServerSideProps({ req }) {
   const { db } = await connectToDatabase();
-  const session = await getSession( { req } );
+  const session = await getSession({ req });
 
   const players = await db
-    .collection( "players" )
-    .find( {} )
-    .sort( { team: 1, name: 1 } )
-    .limit( 200 )
+    .collection("players")
+    .find({})
+    .sort({ team: 1, name: 1 })
+    .limit(200)
     .toArray();
 
   const currentUser = await db
-    .collection( "users" )
-    .findOne( ObjectId( session.userId ) );
+    .collection("users")
+    .findOne(ObjectId(session.userId));
 
   return {
     props: {
-      currentUser: JSON.parse( JSON.stringify( currentUser ) ),
-      players: JSON.parse( JSON.stringify( players ) ),
+      currentUser: JSON.parse(JSON.stringify(currentUser)),
+      players: JSON.parse(JSON.stringify(players)),
     },
   };
 }
