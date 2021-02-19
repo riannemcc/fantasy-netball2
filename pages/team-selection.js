40f1d748ff92bbe0ea8dc0b5eda5f95f2b871987
Rianne <rianne.mccartney@gousto.co.uk>
2021-02-19 21:33:35 +0000

    Reopen team selection

--- pages/team-selection.js
+++ pages/team-selection.js
@@ -25,10 +25,10 @@ export default function TeamSelectionPage({players = [], currentUser}) {
           </span>
         </div>
       ) : (
-          // <TeamSelection players={players} />
-          <div className="m-6 p-2 bg-green-100 border border-green-400 text-black text-xl font-bold px-4 py-3 rounded relative">
-            Team selection is now closed. Come back next year!
-          </div>
+          <TeamSelection players={players} />
+          // <div className="m-6 p-2 bg-green-100 border border-green-400 text-black text-xl font-bold px-4 py-3 rounded relative">
+          //   Team selection is now closed. Come back next year!
+          // </div>
         )}
     </>
   );
