990194850aba847143d6a18969cb7ac17658ff27
Michael Roger <michael.roger@ovoenergy.com>
2021-02-20 15:27:44 +0000

    Add ability to replace injured players.

--- pages/team-selection.js
+++ pages/team-selection.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connectToDatabase } from "../util/mongodb";
 import { TeamSelection } from "../src/components/TeamSelection";
 import { useCurrentUser } from "../src/hooks/useCurrentUser";
+import { findPlayerById } from "../util/helpers";
 
 export default function TeamSelectionPage({ players = [] }) {
   const { currentUser } = useCurrentUser()
@@ -9,24 +10,35 @@ export default function TeamSelectionPage({ players = [] }) {
     return null
   }
 
+  const hasInjuredPlayers = (currentUser.teamPlayers || []).some(({ playerId }) => {
+    const player = findPlayerById(playerId, players)
+    return player && player.isInjured
+  })
+
+  if (currentUser.teamPlayers && hasInjuredPlayers) {
+    return (
+      <TeamSelection players={players} currentUser={currentUser} isInjuryUpdate />
+    )
+  }
+
+  if (currentUser.teamPlayers) {
+    return (
+      <div
+        className="m-6 p-2 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+        role="alert"
+      >
+        You have already selected your team and cannot amend until the
+        mid-season swap window.
+      </div>
+    )
+  }
+
   return (
-    <>
-      {currentUser && currentUser.teamPlayers ? (
-        <div
-          className="m-6 p-2 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
-          role="alert"
-        >
-          You have already selected your team and cannot amend until the
-          mid-season swap window.
-        </div>
-      ) : (
-          <TeamSelection players={players} currentUser={currentUser} />
-          // <div className="m-6 p-2 bg-green-100 border border-green-400 text-black text-xl font-bold px-4 py-3 rounded relative">
-          //   Team selection is now closed. Come back next year!
-          // </div>
-        )}
-    </>
-  );
+    <TeamSelection players={players} currentUser={currentUser} />
+    // <div className="m-6 p-2 bg-green-100 border border-green-400 text-black text-xl font-bold px-4 py-3 rounded relative">
+    //   Team selection is now closed. Come back next year!
+    // </div>
+  )
 }
 
 export async function getServerSideProps({ req }) {
