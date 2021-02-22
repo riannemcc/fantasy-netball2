4f91c4cf5c5c6fb31941d6588f39517446674c7a
Rianne <rianne.mccartney@gousto.co.uk>
2021-02-22 22:39:32 +0000

    Adding weekly points to stats table

--- src/components/StatsTable/StatsTable.tsx
+++ src/components/StatsTable/StatsTable.tsx
@@ -18,23 +18,23 @@ export const StatsTable = ({ players }: StatsTableProps): ReactElement => {
   }));
 
   return (
-    <div>
+    <div className="w-full max-w-xl">
       {teams.map((team, teamIndex) => {
         return (
-          <div
-            key={`stats-table-team-${team}-${teamIndex}`}
-            className="items-center flex flex-col"
-          >
+          <div key={`stats-table-team-${team}-${teamIndex}`}>
             <h2 className="text-xl text-black font-bold mt-6 mb-2 ml-4">
               {team}
             </h2>
-            <table className="table-auto ml-2 mr-2 w-auto shadow-lg bg-gray-100 ">
+            <table className="bg-gray-100 shadow border w-full">
               <thead>
                 <tr>
                   <th className="border border-black px-4 py-2">Player</th>
                   <th className="border border-black px-4 py-2">
                     Total Points
                   </th>
+                  <th className="border border-black px-4 py-2">Game One</th>
+                  <th className="border border-black px-4 py-2">Game Two</th>
+                  <th className="border border-black px-4 py-2">Game Three</th>
                 </tr>
               </thead>
               <tbody>
@@ -47,6 +47,19 @@ export const StatsTable = ({ players }: StatsTableProps): ReactElement => {
                       </td>
                       <td className="border border-black px-4 py-2">
                         {player.points}
+                      </td>
+                      <td className="border border-black px-4 py-2">
+                        {player.games && player.games[0].points}
+                      </td>
+                      <td className="border border-black px-4 py-2">
+                        {player.games &&
+                          player.games[1] &&
+                          player.games[1].points}
+                      </td>
+                      <td className="border border-black px-4 py-2">
+                        {player.games &&
+                          player.games[2] &&
+                          player.games[2].points}
                       </td>
                     </tr>
                   ))}
