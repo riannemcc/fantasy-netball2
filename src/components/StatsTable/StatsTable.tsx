e1c658b9cfed482193329980f7b3f5b4aa98a123
Rianne <rianne.mccartney@gousto.co.uk>
2021-03-09 19:04:28 +0000

    Updating stats table columns

--- src/components/StatsTable/StatsTable.tsx
+++ src/components/StatsTable/StatsTable.tsx
@@ -1,6 +1,7 @@
 import { calculatePlayerPoints } from '_util/helpers';
 import { ReactElement } from 'react';
 import { Player } from '_src/types/players';
+import moment from 'moment';
 
 interface StatsTableProps {
   players: Player[];
@@ -35,34 +36,55 @@ export const StatsTable = ({ players }: StatsTableProps): ReactElement => {
                   <th className="border border-black px-4 py-2">Game One</th>
                   <th className="border border-black px-4 py-2">Game Two</th>
                   <th className="border border-black px-4 py-2">Game Three</th>
+                  <th className="border border-black px-4 py-2">Game Four</th>
+                  <th className="border border-black px-4 py-2">Game Five</th>
+                  <th className="border border-black px-4 py-2">Game Six</th>
                 </tr>
               </thead>
               <tbody>
                 {playersWithPoints
                   .filter((player) => player.team === team)
-                  .map((player) => (
-                    <tr key={`${team}-${player._id}`}>
-                      <td className="border border-black px-4 py-2">
-                        {player.name}
-                      </td>
-                      <td className="border border-black px-4 py-2">
-                        {player.points}
-                      </td>
-                      <td className="border border-black px-4 py-2">
-                        {player.games && player.games[0].points}
-                      </td>
-                      <td className="border border-black px-4 py-2">
-                        {player.games &&
-                          player.games[1] &&
-                          player.games[1].points}
-                      </td>
-                      <td className="border border-black px-4 py-2">
-                        {player.games &&
-                          player.games[2] &&
-                          player.games[2].points}
-                      </td>
-                    </tr>
-                  ))}
+                  .map((player) => {
+                    const games =
+                      player.games &&
+                      player.games.sort((a, b) => {
+                        if (moment(a.startDateTime).isBefore(b.startDateTime)) {
+                          return -1;
+                        }
+                        if (moment(a.startDateTime).isAfter(b.startDateTime)) {
+                          return 1;
+                        }
+                        return 0;
+                      });
+                    return (
+                      <tr key={`${team}-${player._id}`}>
+                        <td className="border border-black px-4 py-2">
+                          {player.name}
+                        </td>
+                        <td className="border border-black px-4 py-2">
+                          {player.points}
+                        </td>
+                        <td className="border border-black px-4 py-2">
+                          {games && games[0] && games[0].points}
+                        </td>
+                        <td className="border border-black px-4 py-2">
+                          {games && games[1] && games[1].points}
+                        </td>
+                        <td className="border border-black px-4 py-2">
+                          {games && games[2] && games[2].points}
+                        </td>
+                        <td className="border border-black px-4 py-2">
+                          {games && games[3] && games[3].points}
+                        </td>
+                        <td className="border border-black px-4 py-2">
+                          {games && games[4] && games[4].points}
+                        </td>
+                        <td className="border border-black px-4 py-2">
+                          {games && games[5] && games[5].points}
+                        </td>
+                      </tr>
+                    );
+                  })}
               </tbody>
             </table>
           </div>
