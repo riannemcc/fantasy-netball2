3882b9e6936b5b5f90ede977a7b8407419a10fd8
Rianne <rianne.mccartney@gousto.co.uk>
2022-02-05 15:59:53 +0000

    remove sponsor comp

--- pages/leaderboard.tsx
+++ pages/leaderboard.tsx
@@ -20,9 +20,9 @@ export default function Leaderboard({
         <h1 className="text-xl text-black font-bold">Leaderboard</h1>
         <div className="border-t-2 flex-1 ml-2 leading-9 text-base font-semibold mt-3.5 border-pink opacity-1 w-6/12" />
       </div>
-      <div className="m-2 mx-auto max-w-2xl flex flex-col items-center">
+      {/* <div className="m-2 mx-auto max-w-2xl flex flex-col items-center">
         <EMMNASponsor />
-      </div>
+      </div> */}
       <div className="flex flex-col items-center m-4 overflow-x-scroll">
         {users ? (
           <LeaderboardTable users={users} players={players} tenRows={false} />
